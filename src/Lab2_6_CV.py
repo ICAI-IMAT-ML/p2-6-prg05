96add023d90a0cde0f99b1babf06684cbed34234
prg05 <202314114@alu.comillas.edu>
2025-04-09 13:01:01 +0200

    nuevo commit

--- src/Lab2_6_CV.py
+++ src/Lab2_6_CV.py
@@ -1,3 +1,4 @@
+import numpy as np
 def cross_validation(model, X, y, nFolds):
     """
     Perform cross-validation on a given machine learning model to evaluate its performance.
@@ -45,25 +46,39 @@ def cross_validation(model, X, y, nFolds):
         nFolds = X.shape[0]
 
     # TODO: Calculate fold_size based on the number of folds
-    fold_size = None
+    fold_size = len(X) // nFolds
+    resto = len(X) % nFolds #Nos aseguramos que todas las observaciones se usen
+
 
     # TODO: Initialize a list to store the accuracy values of the model for each fold
     accuracy_scores = []
-
+    indice_inicial = 0
     for i in range(nFolds):
         # TODO: Generate indices of samples for the validation set for the fold
-        valid_indices = None
+        if i < resto: 
+            suma_resto = 1
+        else: 
+            suma_resto = 0
+        indice_final = indice_inicial + fold_size + suma_resto
+
+        valid_indices = np.arange(indice_inicial,indice_final) #indices del grupo donde se realizará validación
 
         # TODO: Generate indices of samples for the training set for the fold
-        train_indices = None
+        train_1 = np.arange(0,indice_inicial)
+        train_2 = np.arange(indice_final,len(X))
+        train_indices = np.concatenate((train_1, train_2))
 
         # TODO: Split the dataset into training and validation
-        X_train, X_valid = None, None
-        y_train, y_valid = None, None
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
 
         # TODO: Train the model with the training set
-
+        model.fit(X_train, y_train)
         # TODO: Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+        accuracy = model.score(X_valid,y_valid)
+        accuracy_scores.append(accuracy)
+
+        indice_inicial = indice_final #Actualizamos indice para los siguientes nFolds; que el inicio sea el final del anterior
 
     # TODO: Return the mean and standard deviation of the accuracy_scores
-    return None, None
+    return np.mean(accuracy_scores), np.std(accuracy_scores)
